9efa0bc790e397421d3bdd741b4e14fa4a4ca1ee
Niilo <then1ppe@gmail.com>
2018-04-23 14:33:00 +0300

    Creating new repo + Deleted useless stuff + Rescale textures

---
+++ Assets/Scriptit/AirshipPortals.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirshipPortals : MonoBehaviour {
+
+    public GameObject player, pet;
+    public GameObject TeleportLocation;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        pet = GameObject.FindWithTag("Pet");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            player.transform.position = TeleportLocation.transform.position;
+            pet.transform.position = TeleportLocation.transform.position;
+        }
+    }
+}
